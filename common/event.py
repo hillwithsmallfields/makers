4689a06e3ec8a61a97f9b539f83b0fe0045581f3
John Sturdy <jcg.sturdy@gmail.com>
2018-04-02 21:08:45 +0100

    Start to fill in more event stuff

--- common/event.py
+++ common/event.py
@@ -1,9 +1,26 @@
 
 class Event(object):
 
-    def __init__(self, event_type, event_datetime):
+    def __init__(self, event_type,
+                 event_datetime, event_duration):
         """Create an event of a given type and datetime.
-        The current user is added as a host."""
+        The current user is added as a host.
+        The event type names a template to build the event from.
+        An event template is an event which is copied.
+        The event is not saved and scheduled yet."""
+        self.details = get_event_template(event_type)
+        self.details['start'] = event_datetime,
+        self.details['duration'] = event_duration
+        # 'hosts': [], # todo: put the current user in as a host
+        # 'attendees': [],
+        # # preconditions; not sure of the format of this
+        # 'prerequisites': [],
+        # # What the event will result in, if training
+        # 'aim': None,      # what role the event trains for
+        # 'equipment_classes': [],
+        # # What the event needs to reserve
+        # 'location': None,
+        # 'equipment': []
         pass
 
     def get_details(self):
@@ -15,7 +32,7 @@ class Event(object):
         pass
 
     def schedule(self):
-        """Add the event to the list of scheduled events."""
+        """Save the event to the database, and add it to the list of scheduled events."""
         pass
 
     def cancel(self):
@@ -49,3 +66,12 @@ class Event(object):
     def mark_results(self, *attendees):
         """Record the results of a training session."""
         pass
+
+    def save_as_template(self, name):
+        """Save this event as a named template."""
+        pass
+
+def get_event_template(name):
+    """Find an event template by name."""
+    # todo: there should be a collection of event templates in the database
+    return None
