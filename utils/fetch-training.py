9cb9abe9c7df45e1a1d607061521b89bc0b98177
John Sturdy <jcg.sturdy@gmail.com>
2018-04-09 22:21:02 +0100

    start collecting the training URLs

---
+++ utils/fetch-training.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# http://wiki.makespace.org/Equipment/3D_Printer/UM2Training#Trained_Users
+# http://wiki.makespace.org/Equipment/Laser_Cutter/Training#Trained_Users
+# http://wiki.makespace.org/Form_One_Authorised_Users
+# http://wiki.makespace.org/Equipment/CNC_Model_Mill/Training#Trained_Users
+# http://wiki.makespace.org/Equipment/MitreSawRage3Db/Training#Trained_Users
+
