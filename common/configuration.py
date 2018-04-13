4157dae2b9b32ad3439e0f09703f4424852db009
John Sturdy <jcg.sturdy@gmail.com>
2018-04-13 21:17:37 +0100

    cache the config

--- common/configuration.py
+++ common/configuration.py
@@ -3,10 +3,15 @@
 import os
 import yaml
 
+cached_config = None
+
 def get_config():
     """Read the config partly from file and perhaps some from the DB."""
-    file = os.path.expanduser("~/makers-data/makers.yaml")
-    if not os.path.exists(file):
-        file = "/usr/local/share/makers.yaml"
-    with open(file, 'r') as confstream:
-        return yaml.load(confstream)
+    global cached_config
+    if cached_config is None:
+        file = os.path.expanduser("~/makers-data/makers.yaml")
+        if not os.path.exists(file):
+            file = "/usr/local/share/makers.yaml"
+        with open(file, 'r') as confstream:
+            cached_config = yaml.load(confstream)
+    return cached_config
