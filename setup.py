6f4f5cd3c7c0fa45b6ed0571b0d4d1a5c1b6082d
Jnxy <jnxy@lostwire.net>
2018-04-14 12:50:02 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+""" initial one, needs tidying up"""
+setup(name='makers',
+    version='0.0.1',
+    description='makers',
+    author='',
+    author_email='',
+    license='',
+    install_requires = [
+        'pymongo',
+    ],
+    packages=find_packages())
