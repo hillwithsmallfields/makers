b52c35facd90d5dfae57c3a6d503811e83565373
John Sturdy <jcg.sturdy@gmail.com>
2018-05-18 16:02:24 +0100

    Add timeslots code

--- common/timeslots.py
+++ common/timeslots.py
@@ -1,12 +1,40 @@
+#!/usr/bin/python
 
-from nevow import tags as T
-from nevow import flat
+# Represent availability in a week, within a 32-bit integer.
 
-def timeslots_form(config, values):
-    """Make a timeslots form, with some values pre-checked."""
-    times = config['timeslots']['times']
-    slots = len(times)
-    return T.table[T.tr[T.td[""],
-                        [T.th[slot] for slot in slots]],
-                   [T.tr[T.th[day]]
-                            for day in config['timeslots']['days']]]
+# Each four bits represents one day's time slots (morning, afternoon,
+# evening, optinally other).
+
+# When an event is scheduled, it's then easy to search for who may be
+# able to make that time.
+
+def _timeslots_dayparts_to_int(day):
+    return (0 if len(day) == 0
+            else ((1 if day[0] else 0)
+                  | (_timeslots_dayparts_to_int(day[1:]) << 1)))
+
+def timeslots_to_int(timeslots):
+    """Convert a list of lists of available times, to a number.
+    Each sub-list represents four timeslots for one day.
+    The lowest bit of the number is the first timeslot of the first day."""
+    return (0 if len (timeslots) == 0
+            else (_timeslots_dayparts_to_int(timeslots[0])
+                  | (timeslots_to_int(timeslots[1:]) << 4)))
+
+def _timeslots_from_dayparts_number(number):
+    return [ (number & (1 << i)) != 0
+             for i in range(0,4) ]
+
+def timeslots_from_int(number):
+    """Convert a number to a list of lists of available times.
+    Each sub-list represents four timeslots for one day.
+    The lowest bit of the number is the first timeslot of the first day."""
+    return [ _timeslots_from_dayparts_number(number >> (b*4))
+             for b in range(0,7) ]
+
+if __name__ == "__main__":
+    my_number = 0x6744444
+    as_list = timeslots_from_int(my_number)
+    print as_list
+    back = timeslots_to_int(as_list)
+    print back == my_number
