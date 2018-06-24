ebea864959cce4099a36539a12385fb074533cb0
John Sturdy <jcg.sturdy@gmail.com>
2018-06-24 22:14:30 +0100

    More on listing users.

--- pages/user_list_page.py
+++ pages/user_list_page.py
@@ -5,15 +5,25 @@ import person
 
 serverconf=None
 
-def user_list_section(include_non_members=False):
+def user_list_section(include_non_members=False, filter_fn=None):
+    """Return the users list, if the viewing person is allowed to see it.
+    Otherwise, just how many people there are.
+    The optional first argument is a flag for whether to include non-members.
+    The optional second argument is a boolean function taking a person object,
+    returning whether to include them in the list.  This could be used for things
+    like listing people whose fobs are ready for enabling, or who have missed
+    paying their latest subscription."""
     global serverconf
     if serverconf == None:
         serverconf = configuration.get_config()['server']
+    users_base = serverconf['base_url']+serverconf['users']
     # todo: must have done access_permissions.setup_access_permissions(logged_in_user) by now
     permissions = access_permissions.get_access_permissions()
     people = person.Person.list_all_people() if include_non_members else person.Person.list_all_members()
+    if filter_fn:
+        people = [someone for someone in people if filter_fn(someone)]
     if permissions.auditor or permissions.admin:
-        return T.ul[[T.li[who.name] for who in people]]
+        return T.ul[[T.li[T.a(href=users_base+who.link_id)[who.name()]] for who in people]]
     else:
         return T.p["There are "+str(len(people))
                    +(" people" if include_non_members else " members")
