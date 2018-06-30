bf2e103ca6670d7116277a09f0a3d7a10262e175
John Sturdy <jcg.sturdy@gmail.com>
2018-06-30 14:38:46 +0100

    add echos

--- scripts/install-makers.sh
+++ scripts/install-makers.sh
@@ -3,4 +3,7 @@
 SOURCE=${$1-$(pwd)}
 DESTINATION=${2-/var/www/makers}
 
+echo Installing from $SOURCE to $DESTINATION
+
+echo cp $SOURCE/makers/*.py $DESTINATION/makers
 cp $SOURCE/makers/*.py $DESTINATION/makers
