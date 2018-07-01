1e104629f0a126bf5b9d6f977f3fbb1969a651c5
John Sturdy <jcg.sturdy@gmail.com>
2018-07-01 14:57:09 +0100

    import include

--- makers/urls.py
+++ makers/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
