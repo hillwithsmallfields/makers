10a6d258256231d8ced4b4f3efd40e87b5898fa4
John Sturdy <jcg.sturdy@gmail.com>
2018-07-16 22:48:14 +0100

    This is the first version to produce a user dashboard page from a user login

--- pages/person_page.py
+++ pages/person_page.py
@@ -35,7 +35,7 @@ def profile_section(who, viewer):
                    T.tr[T.th(class_="ralabel")[""], T.td["address etc to go here"]]],
                               T.input(type="submit", value="Update details")],
         T.h2["Availability"],
-        page_pieces.availform(who.available)]
+        pages.page_pieces.availform(who.available)]
 
 def responsibilities(who, typename, keyed_types):
     is_owner = who.is_owner(keyed_types[typename])
@@ -43,23 +43,23 @@ def responsibilities(who, typename, keyed_types):
     is_trainer = who.is_trainer(keyed_types[typename])
     has_requested_trainer_training = who.has_requested_training([keyed_types[typename]._id], 'trainer')
     return [T.h3["Machines"],
-            [page_pieces.machinelist(equipment_type.Equipment_type.find(typename),
+            [pages.page_pieces.machinelist(model.equipment_type.Equipment_type.find(typename),
                                      who, is_owner)],
             T.h3["Owner"
                       if is_owner
                       else "Not yet an owner"+(" but has requested owner training" if has_requested_owner_training else "")],
-            T.div(class_='as_owner')[(page_pieces.schedule_event_form(who, [T.input(type="hidden", name="event_type", value="training"),
+            T.div(class_='as_owner')[(pages.page_pieces.schedule_event_form(who, [T.input(type="hidden", name="event_type", value="training"),
                                                                             T.input(type="hidden", name="role", value="owner"),
                                                                             T.input(type="hidden", name="type", value=typename)],
                                                                       "Schedule owner training")
                        if is_owner
-                       else page_pieces.toggle_request(typename, 'owner',
+                       else pages.page_pieces.toggle_request(typename, 'owner',
                                                        has_requested_owner_training))],
             T.h3["Trainer"
                       if is_trainer
                       else "Not yet a trainer"+(" but has requested trainer training" if has_requested_trainer_training else "")],
             # todo: count the training requests for this type, and perhaps what times are most popular
-                 T.div(class_='as_trainer')[(page_pieces.schedule_event_form(who, [T.input(type="hidden", name="event_type", value="training"),
+                 T.div(class_='as_trainer')[(pages.page_pieces.schedule_event_form(who, [T.input(type="hidden", name="event_type", value="training"),
                                                                                    "User training: ", T.input(type="radio",
                                                                                                               name="role",
                                                                                                               value="user",
@@ -71,7 +71,7 @@ def responsibilities(who, typename, keyed_types):
                                                                                    T.input(type="hidden", name="type", value=typename)],
                                                                              "Schedule training")
                        if is_trainer
-                       else page_pieces.toggle_request(typename, 'trainer',
+                       else pages.page_pieces.toggle_request(typename, 'trainer',
                                                        has_requested_trainer_training))]]
 
 def equipment_trained_on(who, equipment_types):
@@ -83,13 +83,13 @@ def equipment_trained_on(who, equipment_types):
     return T.div(class_="trainedon")[
         T.dl[[[T.dt[T.a(href=server_conf['base_url']+server_conf['types']+name)[name]],
                T.dd[ # todo: add when they were trained, and by whom
-                   "Since ", event.timestring(keyed_types[name][1][0].start), T.br,
-                   page_pieces.toggle_request(name, 'trainer',
+                   "Since ", model.event.timestring(keyed_types[name][1][0].start), T.br,
+                   pages.page_pieces.toggle_request(name, 'trainer',
                                               who.has_requested_training([keyed_types[name][0]._id], 'trainer')),
-                   page_pieces.toggle_request(name, 'owner',
+                   pages.page_pieces.toggle_request(name, 'owner',
                                               who.has_requested_training([keyed_types[name][0]._id], 'owner')),
                    # todo: add admin-only buttons for suspending qualifications
-                   page_pieces.machinelist(keyed_types[name][0],
+                   pages.page_pieces.machinelist(keyed_types[name][0],
                                            who, False)]]
                              for name in sorted(keyed_types.keys())]]]
 
@@ -99,17 +99,17 @@ def training_requests_section(who):
     sorted_requests = [keyed_requests[k] for k in sorted(keyed_requests.keys())]
     return T.div(class_="requested")[T.table()[T.tr[T.th["Date"],T.th["Equipment"],T.th["Role"]],
                                                [T.tr[T.td[req['request_date'].strftime("%Y-%m-%d")],
-                                                     T.td[", ".join([equipment_type.Equipment_type.find_by_id(id).pretty_name()
+                                                     T.td[", ".join([model.equipment_type.Equipment_type.find_by_id(id).pretty_name()
                                                                              for id in req['equipment_types']])],
                                                      T.td[str(req['event_type'])[:-len_training]],
-                                                     T.td[page_pieces.cancel_button(",".join(map(str, req['equipment_types'])),
+                                                     T.td[pages.page_pieces.cancel_button(",".join(map(str, req['equipment_types'])),
                                                                                     'user', "Cancel training request")]]
                                                         for req in sorted_requests]]]
 
 
 def admin_section(viewer):
-    return T.ul[T.li[page_pieces.section_link('admin', "userlist", "User list")],
-                T.li[page_pieces.section_link('admin', "intervene", "Create intervention event")]
+    return T.ul[T.li[pages.page_pieces.section_link('admin', "userlist", "User list")],
+                T.li[pages.page_pieces.section_link('admin', "intervene", "Create intervention event")]
                         if viewer.is_administrator() else ""]
 
 def add_person_page_contents(page_data, who, viewer):
@@ -129,50 +129,50 @@ def add_person_page_contents(page_data, who, viewer):
         page_data.add_section("Equipment trained on",
                               equipment_trained_on(who, their_equipment_types))
 
-    all_remaining_types = ((set(equipment_type.Equipment_type.list_equipment_types())
+    all_remaining_types = ((set(model.equipment_type.Equipment_type.list_equipment_types())
                             -their_responsible_types)
                            -their_equipment_types)
     if len(all_remaining_types) > 0:
         page_data.add_section("Other equipment",
-                              page_pieces.general_equipment_list(who, all_remaining_types))
+                              pages.page_pieces.general_equipment_list(who, all_remaining_types))
 
     if len(who.training_requests) > 0:
         page_data.add_section("Training requests", training_requests_section(who))
 
-    hosting = timeline.Timeline.future_events(person_field='hosts', person_id=who._id).events
+    hosting = model.timeline.Timeline.future_events(person_field='hosts', person_id=who._id).events
     if len(hosting) > 0:
         page_data.add_section("Events I'm hosting",
-                              T.div(class_="hostingevents")[page_pieces.eventlist(hosting)])
+                              T.div(class_="hostingevents")[pages.page_pieces.eventlist(hosting)])
 
-    attending = timeline.Timeline.future_events(person_field='attendees', person_id=who._id).events
+    attending = model.timeline.Timeline.future_events(person_field='attendees', person_id=who._id).events
     if len(attending) > 0:
         page_data.add_section("Events I'm attending",
-                              T.div(class_="attendingingevents")[page_pieces.eventlist(attending)])
+                              T.div(class_="attendingingevents")[pages.page_pieces.eventlist(attending)])
 
-    hosted = timeline.Timeline.past_events(person_field='hosts', person_id=who._id).events
+    hosted = model.timeline.Timeline.past_events(person_field='hosts', person_id=who._id).events
     if len(hosting) > 0:
         page_data.add_section("Events I have hosted",
-                              T.div(class_="hostedevents")[page_pieces.eventlist(hosted)])
+                              T.div(class_="hostedevents")[pages.page_pieces.eventlist(hosted)])
 
-    attended = timeline.Timeline.past_events(person_field='attendees', person_id=who._id).events
+    attended = model.timeline.Timeline.past_events(person_field='attendees', person_id=who._id).events
     if len(attended) > 0:
         page_data.add_section("Events I have attended",
-                              T.div(class_="attendedingevents")[page_pieces.eventlist(attended)])
+                              T.div(class_="attendedingevents")[pages.page_pieces.eventlist(attended)])
 
     known_events = hosting + attending + hosted + attended
     available_events = [ev
-                        for ev in timeline.Timeline.future_events().events
+                        for ev in model.timeline.Timeline.future_events().events
                         if ev not in known_events]
     if len(available_events) > 0:
         page_data.add_section("Events I can sign up for",
-                              T.div(class_="availableevents")[page_pieces.eventlist(available_events, True)])
+                              T.div(class_="availableevents")[pages.page_pieces.eventlist(available_events, True)])
 
     # todo: I think this condition is giving false positives
     if viewer.is_administrator() or viewer.is_auditor():
         page_data.add_section("Admin actions",
                               admin_section(viewer))
 
-    userapilink = page_pieces.section_link("userapi", who.link_id, who.link_id)
+    userapilink = pages.page_pieces.section_link("userapi", who.link_id, who.link_id)
     api_link = ["Your user API link is ", userapilink]
     if who.api_authorization is None:
         api_link += [T.br,
