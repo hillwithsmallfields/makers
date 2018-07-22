ff9635aa80841c79ff974f5a0c6f79cb507e406d
John Sturdy <jcg.sturdy@gmail.com>
2018-07-22 12:12:29 +0100

    Enter general list of things to do

--- pages/machine_page.py
+++ pages/machine_page.py
@@ -10,7 +10,9 @@ def machine_section(machine):
     if serverconf == None:
         serverconf = model.configuration.get_config()['server']
     eqty = model.equipment_type.Equipment_type.find_by_id(machine.equipment_type)
+    # todo: owners and admin to be able to set some details
     result = [T.table[T.tr[T.th["Name"], T.td[machine.name]],
                       T.tr[T.th["Type"], T.td[T.a()[eqty.name]]],
+                      # todo: any logged-in user to be able to make status reports
                       T.tr[T.th["Status"], T.td[machine.status]]]]
     return result
