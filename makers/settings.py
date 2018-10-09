fd6c1fb9698b0e91ef1d9b75d00d28212db2c9df
John Sturdy <jcg.sturdy@gmail.com>
2018-10-09 21:57:09 +0100

    Make the secret key secret

--- makers/settings.py
+++ makers/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'w69sqpmjfxnt0arni+(()j5jlzxm#(3!*edxs_@h(#srixrf$$'
+SECRET_KEY = decouple.config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
