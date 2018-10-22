66fe394b00d141f414d500539c8c00787c8be3f4
John Sturdy <jcg.sturdy@gmail.com>
2018-10-22 19:51:29 +0000

    Set the domain in the password reset email

--- model/django_calls.py
+++ model/django_calls.py
@@ -21,12 +21,11 @@ def send_password_reset_email(who, django_request):
     from_email = config['password_reset_from_address']
     to_email = who.get_email()
     domain = config['domain']
-    print("email host is", settings.EMAIL_HOST, "and port is", settings.EMAIL_PORT, "and user is", settings.EMAIL_HOST_USER, "and from_email is", from_email, "and to_email is", to_email)
     form = django.contrib.auth.forms.PasswordResetForm(
         {'email': to_email,
-         'domain': domain,
          'use_https': True
-         # , 'email_template_name': "password-reset-mail.html" # this didn't work
         })
     form.is_valid()
-    form.save(from_email=from_email, request=django_request)
+    form.save(from_email=from_email,
+              domain_override=domain,
+              request=django_request)
